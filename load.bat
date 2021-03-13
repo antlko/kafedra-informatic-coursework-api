1cd6d1e3e65fc9d802010e1782145bf8f032e820
Anatol Kozhukhar <anatol.kozhukhar@paysolut.com>
2021-03-13 22:20:39 +0200

    db

---
+++ load.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+set JDBC_DATABASE_URL="jdbc:postgresql://localhost:5432/"
+set JDBC_DATABASE="postgres"
+set JDBC_USERNAME="postgres"
+set JDBC_PASSWORD="postgres"
+
+PAUSE
--- src/main/java/com/example/configure/DB.java
+++ src/main/java/com/example/configure/DB.java
@@ -14,11 +14,13 @@ public class DB {
     public DataSource getDataSource() {
         Dotenv dotenv = Dotenv.load();
 
+        System.out.println(System.getenv("JDBC_DATABASE_URL"));
+
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url(dotenv.get("JDBC_DATABASE_URL") + dotenv.get("JDBC_DATABASE"));
-        dataSourceBuilder.username(dotenv.get("JDBC_USERNAME"));
-        dataSourceBuilder.password(dotenv.get("JDBC_PASSWORD"));
+        dataSourceBuilder.url(System.getenv("JDBC_DATABASE_URL") + System.getenv("JDBC_DATABASE"));
+        dataSourceBuilder.username(System.getenv("JDBC_USERNAME"));
+        dataSourceBuilder.password(System.getenv("JDBC_PASSWORD"));
         return dataSourceBuilder.build();
     }
 }
