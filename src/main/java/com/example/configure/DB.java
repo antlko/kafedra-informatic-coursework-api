fa819d02680326afdf773f176770c1ae8ebcd5e6
Anatol Kozhukhar <anatol.kozhukhar@paysolut.com>
2021-03-13 22:40:46 +0200

    asd

--- src/main/java/com/example/configure/DB.java
+++ src/main/java/com/example/configure/DB.java
@@ -1,6 +1,5 @@
 package com.example.configure;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +11,13 @@ public class DB {
 
     @Bean
     public DataSource getDataSource() {
-        Dotenv dotenv = Dotenv.load();
+        System.out.println(System.getenv("JDBC_USERNAME"));
 
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url(dotenv.get("JDBC_DATABASE_URL") + dotenv.get("JDBC_DATABASE"));
-        dataSourceBuilder.username(dotenv.get("JDBC_USERNAME"));
-        dataSourceBuilder.password(dotenv.get("JDBC_PASSWORD"));
+        dataSourceBuilder.url(System.getenv("JDBC_DATABASE_URL") + System.getenv("JDBC_DATABASE"));
+        dataSourceBuilder.username(System.getenv("JDBC_USERNAME"));
+        dataSourceBuilder.password(System.getenv("JDBC_PASSWORD"));
         return dataSourceBuilder.build();
     }
 }
